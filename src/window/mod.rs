2b26c487c5bfd721733e14896c49302dd60dc49a
Lyndon Sanche <lsanche@lyndeno.ca>
2024-02-15 14:50:17 -0700

    Switch to gtk image

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -13,6 +13,7 @@ use glib::{clone, Object};
 use gtk::gio::ActionEntry;
 use gtk::glib::property::PropertyGet;
 use gtk::Button;
+use gtk::Image;
 use gtk::ListBox;
 use gtk::{gio, glib, NoSelection, SignalListItemFactory};
 
@@ -98,12 +99,7 @@ impl Window {
     }
 
     fn create_repo_row(&self, repo_object: &RepoObject) -> ActionRow {
-        let button = Button::builder()
-            .icon_name("arrow1-right-symbolic")
-            .can_focus(false)
-            .can_target(false)
-            .has_frame(false)
-            .build();
+        let button = Image::builder().icon_name("go-next-symbolic").build();
         let row = ActionRow::builder().activatable(true).build();
 
         row.add_suffix(&button);
