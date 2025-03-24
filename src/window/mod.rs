a14f70d6899bc8da65ca8fc044cb7bf72b99f8b3
Lyndon Sanche <lsanche@lyndeno.ca>
2025-03-24 14:15:09 -0600

    Update gtk clone macros

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -43,18 +43,26 @@ impl Window {
         let selection_model = NoSelection::new(Some(self.repos()));
         self.imp().repo_list.bind_model(
             Some(&selection_model),
-            clone!(@weak self as window => @default-panic, move |obj| {
-                let repo_object = obj.downcast_ref().expect("Obj should be RepoObject");
-                let row = window.create_repo_row(repo_object);
-                row.upcast()
-            }),
+            clone!(
+                #[weak(rename_to = window)]
+                self,
+                #[upgrade_or_panic]
+                move |obj| {
+                    let repo_object = obj.downcast_ref().expect("Obj should be RepoObject");
+                    let row = window.create_repo_row(repo_object);
+                    row.upcast()
+                }
+            ),
         );
 
         self.set_repo_list_visible(&self.repos());
-        self.repos()
-            .connect_items_changed(clone!(@weak self as window => move |repos, _, _, _| {
+        self.repos().connect_items_changed(clone!(
+            #[weak(rename_to = window)]
+            self,
+            move |repos, _, _, _| {
                 window.set_repo_list_visible(repos);
-            }));
+            }
+        ));
     }
 
     fn set_repo_list_visible(&self, repos: &gio::ListStore) {
@@ -63,32 +71,45 @@ impl Window {
 
     fn setup_callbacks(&self) {
         let (sender, receiver) = async_channel::bounded(1);
-        self.imp()
-            .refresh_button
-            .connect_clicked(clone!(@weak self as window => move |_| {
+        self.imp().refresh_button.connect_clicked(clone!(
+            #[weak(rename_to = window)]
+            self,
+            move |_| {
                 window.imp().refresh_button.set_sensitive(false);
                 window.imp().refresh_spinner.set_visible(true);
-                crate::runtime().spawn(clone!(@strong sender => async move {
-                    let repos = get_repos().await;
-                    sender.send(repos).await.expect("Channel is not open");
-                }));
-            }));
-
-        glib::spawn_future_local(clone!(@weak self as window => async move {
-            while let Ok(response) = receiver.recv().await {
-                let mut total = 0f64;
-                window.repos().remove_all();
-                for repo in response {
-                    total += repo.current_usage.unwrap_or(0f64);
-                    let item = RepoObject::new(repo);
-                    window.repos().append(&item);
-                }
-                window.imp().current_usage.set_label(&format!("{:.2} GB", total / 1000f64));
-                window.imp().refresh_button.set_sensitive(true);
-                window.imp().refresh_spinner.set_visible(false);
-                window.imp().mytoast.add_toast(Toast::new("Refreshed"));
+                crate::runtime().spawn(clone!(
+                    #[strong]
+                    sender,
+                    async move {
+                        let repos = get_repos().await;
+                        sender.send(repos).await.expect("Channel is not open");
+                    }
+                ));
             }
-        }));
+        ));
+
+        glib::spawn_future_local(clone!(
+            #[weak(rename_to = window)]
+            self,
+            async move {
+                while let Ok(response) = receiver.recv().await {
+                    let mut total = 0f64;
+                    window.repos().remove_all();
+                    for repo in response {
+                        total += repo.current_usage.unwrap_or(0f64);
+                        let item = RepoObject::new(repo);
+                        window.repos().append(&item);
+                    }
+                    window
+                        .imp()
+                        .current_usage
+                        .set_label(&format!("{:.2} GB", total / 1000f64));
+                    window.imp().refresh_button.set_sensitive(true);
+                    window.imp().refresh_spinner.set_visible(false);
+                    window.imp().mytoast.add_toast(Toast::new("Refreshed"));
+                }
+            }
+        ));
     }
 
     fn create_repo_row(&self, repo_object: &RepoObject) -> ActionRow {
@@ -97,11 +118,17 @@ impl Window {
 
         row.add_suffix(&button);
 
-        row.connect_activated(clone!(@weak self as window, @weak repo_object => move |_| {
-            let page = RepoPage::new();
-            page.bind(&repo_object);
-            window.imp().repo_view.push(&page);
-        }));
+        row.connect_activated(clone!(
+            #[weak(rename_to = window)]
+            self,
+            #[weak]
+            repo_object,
+            move |_| {
+                let page = RepoPage::new();
+                page.bind(&repo_object);
+                window.imp().repo_view.push(&page);
+            }
+        ));
 
         repo_object
             .bind_property("name", &row, "title")
